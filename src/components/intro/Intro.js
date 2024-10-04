ddf5eb4a10c9574752a81428cc58c04afd9dd096
JerichoMontecillo <jerichomontec@csu.fullerton.edu>
2024-10-03 18:38:15 -0700

    Fixed intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -9,7 +9,7 @@ const Intro = () =>  {
         <section id="intro">
             <div className="introContent">
                 <span className="helloMessage">Hello,</span>
-                <span className="introMessage">I am <span className="introName">Jericho</span> <br />THE Computer Scientist</span>
+                <span className="introMessage">I am <span className="introName">Jericho</span></span>
                 <p className="introParagraph">I am a motivated Computer Scientist from California State University of Fullerton. <br/> I enjoy playing and developing video games, puzzles, and really anything that works the mind.</p>
                 <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-50}><button className="button"><img src={hire} alt="Hire Me" className="buttonImage" />Hire Me</button></Link>
             </div>
